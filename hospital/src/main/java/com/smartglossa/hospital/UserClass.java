804f90599e1b581f9126d35480c9f4a250fcb2f4
venkat@smarglossa.com <priyaanbu1>
2016-11-04 18:45:52 +0530

    adding doctor and patient details

--- hospital/src/main/java/com/smartglossa/hospital/UserClass.java
+++ hospital/src/main/java/com/smartglossa/hospital/UserClass.java
@@ -123,4 +123,232 @@ public class UserClass {
 				
         	  return result;
           }
+          public JSONObject addDoctor(String fullName, String userName, String pass, String gender, String specialist, String mobileNumber, String place, String city, String state, String country, String pincode) throws JSONException{
+        	JSONObject result = new JSONObject(); 
+        	try{
+        	Class.forName("com.mysql.jdbc.Driver");
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospitalmanagement", "root", "root");
+	        Statement statement = connection.createStatement();
+	        String query = "Insert into doctor(fullName,userName,pass,gender,specialist,mobileNumber,place,city,state,country,pincode) values('"+ fullName +"','"+ userName +"','"+ pass +"','"+ gender +"','"+ specialist +"','"+ mobileNumber +"','"+ place +"','"+ city +"','"+ country +"','"+ pincode +"')";
+	        statement.execute(query);
+	        result.put("Status","1");
+        	}catch(Exception e){
+        		JSONObject error = new JSONObject();
+        		error.put("Status","0");
+        		error.put("Message","Internal Error");
+        		return error;
+        	}
+        	return result;
+          }
+          public JSONObject updateDoctor(String fullName, String userName, String pass, String gender, String specialist, String mobileNumber, String place, String city, String state, String country, String pincode) throws JSONException{
+        	  JSONObject result = new JSONObject();
+        	  try {
+				Class.forName("com.mysql.jdbc.Driver");
+				Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospitalmanagement", "root", "root");
+	  	        Statement statement = connection.createStatement();
+	  	        String query = "update doctor set fullName='"+ fullName +"',pass='"+ pass +"',gender='"+ gender +"',specialist='"+ specialist +"',mobileNumber='"+ mobileNumber +"',place='"+ place +"',city='"+ city +"',country='"+ country +"',pincode='"+ pincode +"' where userName='"+userName;
+	  	        statement.execute(query);
+	  	        result.put("Status", "1");
+			} catch (Exception e) {
+				JSONObject error = new JSONObject();
+        		error.put("Status","0");
+        		error.put("Message","Internal Error");
+        		return error;
+				
+			}
+        	  return result;
+  			
+          }
+          public JSONObject deleteDoctor(String userName)throws JSONException{
+        	  JSONObject result = new JSONObject();
+        	    try {
+					Class.forName("com.mysql.jdbc.Driver");
+					Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospitalmanagement", "root", "root");
+		  	        Statement statement = connection.createStatement();
+		  	        String query = "delete from doctor where userName='"+ userName;
+		  	        statement.execute(query);
+		  	        result.put("Status", "1");
+				} catch (Exception e) {
+					
+					JSONObject error = new JSONObject();
+	        		error.put("Status","0");
+	        		error.put("Message","Internal Error");
+	        		return error;
+				}
+				
+	  	        
+        	  return result;
+          }
+          public JSONObject getDoctordetail(String userName)throws JSONException{
+        	  JSONObject result = new JSONObject();
+        	  try {
+				Class.forName("com.mysql.jdbc.Driver");
+				Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospitalmanagement", "root", "root");
+	  	        Statement statement = connection.createStatement();
+	  	        String query = "select *from doctor where userName='"+userName;
+	  	        ResultSet rs = statement.executeQuery(query);
+	  	        if(rs.next()){
+	  	        	result.put("fullName",rs.getString("fullName"));
+	  	        	result.put("userName", rs.getString("userName"));
+	  	        	result.put("gender", rs.getString("gender"));
+	  	        	result.put("specialist", rs.getString("specialist"));
+	  	        	result.put("mobileNumber", rs.getString("mobileNumber"));
+	  	        	result.put("place", rs.getString("place"));
+	  	        	result.put("city", rs.getString("city"));
+	  	        	result.put("state", rs.getString("state"));
+	  	        	result.put("country", rs.getString("country"));
+	  	        	result.put("pincode", rs.getString("pincode"));
+	  	        }
+			} catch (Exception e) {
+				JSONObject error = new JSONObject();
+        		error.put("Status","0");
+        		error.put("Message","Internal Error");
+        		return error;
+			}
+				
+        	  return result;
+          }
+          public JSONArray getAllDoctors()throws JSONException{
+        	  JSONArray result = new JSONArray();
+        	  try {
+				Class.forName("com.mysql.jdbc.Driver");
+				Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospitalmanagement", "root", "root");
+	  	        Statement statement = connection.createStatement();
+	  	        String query = "select *from doctor";
+	  	        ResultSet rs = statement.executeQuery(query);
+	  	        if(rs.next()){
+	  	        	JSONObject user = new JSONObject(); 
+	  	        	user.put("fullName",rs.getString("fullName"));
+	  	        	user.put("userName", rs.getString("userName"));
+	  	        	user.put("gender", rs.getString("gender"));
+	  	        	user.put("specialist", rs.getString("specialist"));
+	  	        	user.put("mobileNumber", rs.getString("mobileNumber"));
+	  	        	user.put("place", rs.getString("place"));
+	  	        	user.put("city", rs.getString("city"));
+	  	        	user.put("state", rs.getString("state"));
+	  	        	user.put("country", rs.getString("country"));
+	  	        	user.put("pincode", rs.getString("pincode"));
+	  	        	result.put(user);
+	  	        }
+			} catch (Exception e) {
+				JSONArray error = new JSONArray();
+        		error.put("Internal Error");
+        	  	return error;
+			}
+				
+        	  return result;
+          }
+          public JSONObject addPatient(String fullName, String gender, String mobileNumber, String place, String city, String state, String country, String pincode) throws JSONException{
+          	JSONObject result = new JSONObject(); 
+          	try{
+          	Class.forName("com.mysql.jdbc.Driver");
+  			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospitalmanagement", "root", "root");
+  	        Statement statement = connection.createStatement();
+  	        String query = "Insert into patient(fullName,gender,mobileNumber,place,city,state,country,pincode) values('"+ fullName +"','"+ gender +"','"+ mobileNumber +"','"+ place +"','"+ city +"','"+ country +"','"+ pincode +"')";
+  	        statement.execute(query);
+  	        result.put("Status","1");
+          	}catch(Exception e){
+          		JSONObject error = new JSONObject();
+          		error.put("Status","0");
+          		error.put("Message","Internal Error");
+          		return error;
+          	}
+          	return result;
+            }
+            public JSONObject updatePatient(int id,String fullName, String gender, String mobileNumber, String place, String city, String state, String country, String pincode) throws JSONException{
+          	  JSONObject result = new JSONObject();
+          	  try {
+  				Class.forName("com.mysql.jdbc.Driver");
+  				Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospitalmanagement", "root", "root");
+  	  	        Statement statement = connection.createStatement();
+  	  	        String query = "update patient set fullName='"+ fullName +"',gender='"+ gender +"',mobileNumber='"+ mobileNumber +"',place='"+ place +"',city='"+ city +"',country='"+ country +"',pincode='"+ pincode +"' where id='"+id;
+  	  	        statement.execute(query);
+  	  	        result.put("Status", "1");
+  			} catch (Exception e) {
+  				JSONObject error = new JSONObject();
+          		error.put("Status","0");
+          		error.put("Message","Internal Error");
+          		return error;
+  				
+  			}
+          	  return result;
+    			
+            }
+            public JSONObject deletePatient(int id)throws JSONException{
+          	  JSONObject result = new JSONObject();
+          	    try {
+  					Class.forName("com.mysql.jdbc.Driver");
+  					Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospitalmanagement", "root", "root");
+  		  	        Statement statement = connection.createStatement();
+  		  	        String query = "delete from patient where id='"+ id;
+  		  	        statement.execute(query);
+  		  	        result.put("Status", "1");
+  				} catch (Exception e) {
+  					
+  					JSONObject error = new JSONObject();
+  	        		error.put("Status","0");
+  	        		error.put("Message","Internal Error");
+  	        		return error;
+  				}
+  				
+  	  	        
+          	  return result;
+            }
+            public JSONObject getPatientdetail(int id)throws JSONException{
+          	  JSONObject result = new JSONObject();
+          	  try {
+  				Class.forName("com.mysql.jdbc.Driver");
+  				Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospitalmanagement", "root", "root");
+  	  	        Statement statement = connection.createStatement();
+  	  	        String query = "select *from patient where id='"+id;
+  	  	        ResultSet rs = statement.executeQuery(query);
+  	  	        if(rs.next()){
+  	  	            result.put("id", rs.getInt("id"));
+  	  	        	result.put("fullName",rs.getString("fullName"));
+  	  	         	result.put("gender", rs.getString("gender"));
+  	  	        	result.put("mobileNumber", rs.getString("mobileNumber"));
+  	  	        	result.put("place", rs.getString("place"));
+  	  	        	result.put("city", rs.getString("city"));
+  	  	        	result.put("state", rs.getString("state"));
+  	  	        	result.put("country", rs.getString("country"));
+  	  	        	result.put("pincode", rs.getString("pincode"));
+  	  	        }
+  			} catch (Exception e) {
+  				JSONObject error = new JSONObject();
+          		error.put("Status","0");
+          		error.put("Message","Internal Error");
+          		return error;
+  			}
+  				
+          	  return result;
+            }
+            public JSONArray getAllPatients()throws JSONException{
+          	  JSONArray result = new JSONArray();
+          	  try {
+  				Class.forName("com.mysql.jdbc.Driver");
+  				Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospitalmanagement", "root", "root");
+  	  	        Statement statement = connection.createStatement();
+  	  	        String query = "select *from patient";
+  	  	        ResultSet rs = statement.executeQuery(query);
+  	  	        if(rs.next()){
+  	  	        	JSONObject patient = new JSONObject(); 
+  	  	            patient.put("id", rs.getInt("id"));
+  	  	        	patient.put("fullName",rs.getString("fullName"));
+  	  	        	patient.put("gender", rs.getString("gender"));
+  	  	        	patient.put("mobileNumber", rs.getString("mobileNumber"));
+  	  	        	patient.put("place", rs.getString("place"));
+  	  	        	patient.put("city", rs.getString("city"));
+  	  	        	patient.put("state", rs.getString("state"));
+  	  	        	patient.put("country", rs.getString("country"));
+  	  	        	patient.put("pincode", rs.getString("pincode"));
+  	  	        	result.put(patient);
+  	  	        }
+  			} catch (Exception e) {
+  				JSONArray error = new JSONArray();
+          		error.put("Internal Error");
+          	  	return error;
+  			}
+  				
+          	  return result;
+            }
 }
--- hospital/src/main/java/com/smartglossa/hospital/UserServlet.java
+++ hospital/src/main/java/com/smartglossa/hospital/UserServlet.java
@@ -93,7 +93,159 @@ public class UserServlet extends HttpServlet {
 			}
 			
 					
+		}else if(operation.equals("6")){
+			String fullName = req.getParameter("fullName");
+			String userName = req.getParameter("userName");
+			String pass = req.getParameter("pass");
+			String gender = req.getParameter("gender");
+			String specialist = req.getParameter("specialist");
+			String mobileNumber = req.getParameter("mobileNumber");
+			String place = req.getParameter("place");
+			String city = req.getParameter("city");
+			String state = req.getParameter("state");
+			String country = req.getParameter("country");
+			String pincode = req.getParameter("pincode");
+			
+			JSONObject result;
+			try {
+				result = userObject.addDoctor(fullName, userName, pass, gender, specialist, mobileNumber, place, city, state, country, pincode);
+				res.getWriter().print(result);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}else if(operation.equals("7")){
+			String fullName = req.getParameter("fullName");
+			String userName = req.getParameter("userName");
+			String pass = req.getParameter("pass");
+			String gender = req.getParameter("gender");
+			String specialist = req.getParameter("specialist");
+			String mobileNumber = req.getParameter("mobileNumber");
+			String place = req.getParameter("place");
+			String city = req.getParameter("city");
+			String state = req.getParameter("state");
+			String country = req.getParameter("country");
+			String pincode = req.getParameter("pincode");
+			
+			JSONObject result;
+			try {
+				result = userObject.updateDoctor(fullName, userName, pass, gender, specialist, mobileNumber, place, city, state, country, pincode);
+				res.getWriter().print(result);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}else if(operation.equals("8")){
+			String userName =req.getParameter("userName");
+			
+			JSONObject result;
+			try {
+				result = userObject.deleteDoctor(userName);
+				res.getWriter().print(result);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else if(operation.equals("9")){
+			String userName = req.getParameter("userName");
+			
+			JSONObject result;
+			try {
+				result = userObject.getDoctordetail(userName);
+				res.getWriter().print(result);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else if(operation.equals("10")){
+			JSONArray result;
+			try {
+				result = userObject.getAllDoctors();
+				res.getWriter().print(result);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+					
+		
+		}else if(operation.equals("11")){
+			String fullName = req.getParameter("fullName");
+		    String gender = req.getParameter("gender");
+		    String mobileNumber = req.getParameter("mobileNumber");
+			String place = req.getParameter("place");
+			String city = req.getParameter("city");
+			String state = req.getParameter("state");
+			String country = req.getParameter("country");
+			String pincode = req.getParameter("pincode");
+			
+			JSONObject result;
+			try {
+				result = userObject.addPatient(fullName, gender, mobileNumber, place, city, state, country, pincode);
+				res.getWriter().print(result);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}else if(operation.equals("12")){
+			int id = Integer.parseInt(req.getParameter("id"));
+			String fullName = req.getParameter("fullName");
+			String gender = req.getParameter("gender");
+			String mobileNumber = req.getParameter("mobileNumber");
+			String place = req.getParameter("place");
+			String city = req.getParameter("city");
+			String state = req.getParameter("state");
+			String country = req.getParameter("country");
+			String pincode = req.getParameter("pincode");
+			
+			JSONObject result;
+			try {
+				result = userObject.updatePatient(id, fullName, gender, mobileNumber, place, city, state, country, pincode);
+				res.getWriter().print(result);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}else if(operation.equals("13")){
+			int id = Integer.parseInt(req.getParameter("id"));
+			
+			JSONObject result;
+			try {
+				result = userObject.deletePatient(id);
+				res.getWriter().print(result);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else if(operation.equals("14")){
+			int id = Integer.parseInt(req.getParameter("id"));
+			
+			JSONObject result;
+			try {
+				result = userObject.getPatientdetail(id);
+				res.getWriter().print(result);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else if(operation.equals("15")){
+			JSONArray result;
+			try {
+				result = userObject.getAllPatients();
+				res.getWriter().print(result);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+					
+		
 		}
+		
 		
 	}
 
